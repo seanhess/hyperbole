46af2305c89ba1072b9ad350e5f6911ed219722f
Sean Hess <seanhess@gmail.com>
2024-10-01 11:59:37 -0600

    fixed import conflict

--- src/Web/Hyperbole/Forms.hs
+++ src/Web/Hyperbole/Forms.hs
@@ -44,12 +44,11 @@ import Data.Text (Text, pack)
 import Effectful
 import GHC.Generics
 import Text.Casing (kebab)
+import Web.FormUrlEncoded (FormOptions (..), defaultFormOptions, parseUnique)
 import Web.FormUrlEncoded qualified as FE
 import Web.HttpApiData (FromHttpApiData (..))
 import Web.Hyperbole.Effect
-import Web.Hyperbole.HyperView (HyperView (..), ViewAction (..), ViewId (..), dataTarget)
-import Web.Hyperbole.Types (Elem)
-import Web.Internal.FormUrlEncoded (FormOptions (..), defaultFormOptions)
+import Web.Hyperbole.HyperView (HyperView (..), ViewAction (..), dataTarget)
 import Web.View hiding (form, input, label)
 
 
