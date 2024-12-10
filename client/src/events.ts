984d83ce56a0b1a52d967c56fa0f66f13cba62e6
Sean Hess <seanhess@gmail.com>
2024-12-10 09:22:08 -0700

    nearestTarget in events.ts

--- client/src/events.ts
+++ client/src/events.ts
@@ -10,49 +10,36 @@ export function listenClick(cb:(target:HTMLElement, action:string) => void): voi
     // clicks can fire on internal elements. Find the parent with a click handler
     let source = el.closest("[data-on-click]") as HTMLElement
 
-    // console.log("CLICK", source?.dataset.onClick)
+    // Let the click do its thing
+    if (!source) return
 
-    // they should all have an action and target
-    if (source?.dataset.onClick && source?.dataset.target) {
-      e.preventDefault()
-
-      let target = document.getElementById(source.dataset.target)
-
-      if (!target) {
-        console.error("Missing target: ", source.dataset.target)
-        return
-      }
-
-      cb(target, source.dataset.onClick)
-    }
+    e.preventDefault()
+    let target = nearestTarget(source)
+    cb(target, source.dataset.onClick)
   })
 }
 
 
 export function listenLoadDocument(cb:(target:HTMLElement, action:string) => void): void {
-
-  document.addEventListener("hyp-load", function(e) {
+  document.addEventListener("hyp-load", function(e:CustomEvent) {
     let load = e.target as HTMLElement
     let action = load.dataset.onLoad
-    let target = document.getElementById(load.dataset.target)
-
-    if (!target) {
-      console.error("Missing load target: ", target)
-      return
-    }
-
+    let target = e.detail.target
     cb(target, action)
   })
 
 }
 
+
 export function listenLoad(node:HTMLElement): void {
+
   // it doesn't really matter WHO runs this except that it should have target
   node.querySelectorAll("[data-on-load]").forEach((load:HTMLElement) => {
     let delay = parseInt(load.dataset.delay) || 0
 
     setTimeout(() => {
-      let event = new Event("hyp-load", {bubbles:true})
+      const event = new CustomEvent("hyp-load", {bubbles:true, detail: {target: nearestTarget(load)}})
+      console.log("DISPATCH", event)
       load.dispatchEvent(event)
     }, delay)
   })
@@ -66,19 +53,17 @@ export function listenChange(cb:(target:HTMLElement, action:string) => void): vo
     // clicks can fire on internal elements. Find the parent with a click handler
     let source = el.closest("[data-on-change]") as HTMLInputElement
 
+    if (!source) return
+    e.preventDefault()
+
     // they should all have an action and target
-    if (source?.dataset.target && source.value) {
-      e.preventDefault()
-
-      let target = document.getElementById(source.dataset.target)
-
-      if (!target) {
-        console.error("Missing target: ", source.dataset.target)
-        return
-      }
-
-      cb(target, source.value)
+    if (!source.value) {
+      console.error("Missing input value:", source)
+      return
     }
+
+    let target = nearestTarget(source)
+    cb(target, source.value)
   })
 }
 
@@ -90,29 +75,28 @@ export function listenInput(cb:(target:HTMLElement, actionConstructor:string, te
   document.addEventListener("input", function(e) {
     let el = e.target as HTMLElement
     let source = el.closest("[data-on-input]") as LiveInputElement
+
+    if (!source) return
+
     let delay = parseInt(source.dataset.delay) || 100
-
-
     if (delay < 100) {
       console.warn("Input delay < 100 can result in poor performance")
     }
 
-    if (source?.dataset.onInput && source?.dataset.target) {
-      e.preventDefault()
-
-      let target = document.getElementById(source.dataset.target)
-
-      if (!target) {
-        console.error("Missing target: ", source.dataset.target)
-        return
-      }
-
-      if (!source.debouncedCallback) {
-        source.debouncedCallback = debounce(() => cb(target, source.dataset.onInput, source.value), delay)
-      }
-
-      source.debouncedCallback()
+    if (!source?.dataset.onInput) {
+      console.error("Missing onInput: ", source)
+      return
     }
+
+    e.preventDefault()
+
+    let target = nearestTarget(source)
+
+    if (!source.debouncedCallback) {
+      source.debouncedCallback = debounce(() => cb(target, source.dataset.onInput, source.value), delay)
+    }
+
+    source.debouncedCallback()
   })
 }
 
@@ -122,18 +106,32 @@ export function listenFormSubmit(cb:(target:HTMLElement, action:string, form:For
     let form = e.target as HTMLFormElement
 
     // they should all have an action and target
-    if (form?.dataset.onSubmit && form?.dataset.target) {
-      e.preventDefault()
-
-      let target = document.getElementById(form.dataset.target)
-
-      if (!target) {
-        console.error("Missing target: ", form.dataset.target)
-        return
-      }
-
-      const formData = new FormData(form)
-      cb(target, form.dataset.onSubmit, formData)
+    if (!form?.dataset.onSubmit) {
+      console.error("Missing onSubmit: ", form)
+      return
     }
+
+    e.preventDefault()
+
+    let target = nearestTarget(form)
+    const formData = new FormData(form)
+    cb(target, form.dataset.onSubmit, formData)
   })
+}
+
+function nearestTargetId (node:HTMLElement):string | undefined {
+  let targetData = node.closest("[data-target]") as HTMLElement | undefined
+  return targetData?.dataset.target || node.closest("[id]")?.id
+}
+
+function nearestTarget(node:HTMLElement):HTMLElement {
+  let targetId = nearestTargetId(node)
+  let target = document.getElementById(targetId)
+
+  if (!target) {
+    console.error("Cannot find target: ", node)
+    return
+  }
+
+  return target
 }
