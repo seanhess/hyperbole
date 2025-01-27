47e39087354a866cc2b5f9888cc9fcb66114abb4
Sean Hess <seanhess@gmail.com>
2025-01-27 09:45:59 -0700

    fixed multiple  history.replaceState()

--- client/src/browser.ts
+++ client/src/browser.ts
@@ -1,10 +1,14 @@
 
 export function setQuery(query: string = "") {
-
-  if (location.search != query) {
+  if (query != currentQuery()) {
     if (query != "") query = "?" + query
     let url = location.pathname + query
     console.log("history.replaceState(", url, ")")
     window.history.replaceState({}, "", url)
   }
+}
+
+function currentQuery(): string {
+  const query = window.location.search;
+  return query.startsWith('?') ? query.substring(1) : query;
 }
