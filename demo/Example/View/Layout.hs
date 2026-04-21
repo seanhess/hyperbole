0f88d92bc9529b470a0b2ae00ebc064c81482b1c
jk <47693+sectore@users.noreply.github.com>
2026-04-21 20:07:02 +0200

    demo: add gh + hackage icons

--- demo/Example/View/Layout.hs
+++ demo/Example/View/Layout.hs
@@ -12,7 +12,7 @@ import Data.Version (showVersion)
 import Example.Colors (AppColor (..))
 import Example.Style qualified as Style
 import Example.Style.Cyber qualified as Cyber
-import Example.View.Icon as Icon (hamburger)
+import Example.View.Icon as Icon (github, hamburger, haskell)
 import Example.View.Menu (menu)
 import Paths_demo (version)
 import Web.Atomic.CSS
@@ -46,16 +46,23 @@ navigation :: View c () -> View c ()
 navigation chosenMenu = do
   nav ~ bg Dark . color White . flexCol . showMenuHover $ do
     row $ do
-      link [uri|https://hackage.haskell.org/package/hyperbole|] "HYPERBOLE" ~ bold . pad 20 . logo . width 220
+      link hackageUrl "HYPERBOLE" ~ bold . pad 20 . logo . width 220
       space
       menuButton
     col ~ cls "menu" . onMobile (display None) . Cyber.font . Style.uppercase $ do
       chosenMenu
       space
-      el ~ pad 10 . fontSize 12 $ do
-        text "v"
-        text $ cs $ showVersion version
+      row ~ pad (TL 20 10) . gap 10 . utility "items-center" ["align-items" :. "center"] $ do
+        el ~ fontSize 12 $ do
+          text "v"
+          text $ cs $ showVersion version
+        row $ do
+          link hackageUrl (el ~ width 20 . height 20 . flexRow . utility "items-center" ["align-items" :. "center"] $ Icon.haskell) ~ pad 8 . hover (bg DarkHighlight)
+          link githubUrl (el ~ width 20 . height 20 . flexRow . utility "items-center" ["align-items" :. "center"] $ Icon.github) ~ pad 8 . hover (bg DarkHighlight)
  where
+  hackageUrl = [uri|https://hackage.haskell.org/package/hyperbole|]
+  githubUrl = [uri|https://github.com/seanhess/hyperbole|]
+
   menuButton =
     el ~ onDesktop (display None) . onMobile flexCol $ do
       el ~ pad 6 $ do
