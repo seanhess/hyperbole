cab59260463656910333475051e37245787f30ae
jk <47693+sectore@users.noreply.github.com>
2026-05-28 14:46:56 +0200

    example(table): extend to reflect sort directions

--- demo/Example/DataLists/DataTable.hs
+++ demo/Example/DataLists/DataTable.hs
@@ -6,13 +6,15 @@ module Example.DataLists.DataTable where
 import App.Docs
 import App.Route as Route
 import Data.List (sortOn)
+import Data.Ord (Down (Down))
 import Data.Text (pack)
 import Effectful
 import Example.Data.ProgrammingLanguage (ProgrammingLanguage (..), allLanguages)
 import Example.View.Layout
-import Example.View.SortableTable (dataTable, sortBtn, sortColumn)
+import Example.View.SortableTable (SortDirection (..), dataTable, sortBtn, sortColumn)
 import Web.Atomic.CSS
 import Web.Hyperbole
+import Web.Hyperbole.HyperView
 import Prelude hiding (even, odd)
 
 -- DataTable -> do
@@ -23,10 +25,13 @@ page = do
   pure $ layout (Data SortableTable) $ do
     el "We can write view Functions that work in any view, like a dataTable"
     example $(moduleSource) $ do
-      hyper Languages $ languagesView Nothing allLanguages
+      hyperState Languages Nothing $ languagesView allLanguages
 
 data Languages = Languages
-  deriving (Generic, ViewId)
+  deriving (Generic)
+
+instance ViewId Languages where
+  type ViewState Languages = Maybe (SortField, SortDirection)
 
 data SortField
   = SortName
@@ -34,24 +39,43 @@ data SortField
   | SortFamily
   deriving (Show, Read, Eq, Generic, ToJSON, FromJSON)
 
+toggleSortDir :: SortDirection -> SortDirection
+toggleSortDir Ascending = Descending
+toggleSortDir Descending = Ascending
+
 instance HyperView Languages es where
   data Action Languages
     = SortOn SortField
     deriving (Generic, ViewAction)
 
   update (SortOn fld) = do
-    let sorted = sortOnField fld allLanguages
-    pure $ languagesView (Just fld) sorted
+    newDir <- gets $ \case
+      Just (currFld, currDir) | currFld == fld -> toggleSortDir currDir
+      _ -> Ascending
+    put $ Just (fld, newDir)
+    let sorted = sortOnField fld newDir allLanguages
+    pure $ languagesView sorted
 
-sortOnField :: SortField -> [ProgrammingLanguage] -> [ProgrammingLanguage]
-sortOnField = \case
-  SortName -> sortOn (.name)
-  SortDescription -> sortOn (.description)
-  SortFamily -> sortOn (.family)
+sortOnField :: SortField -> SortDirection -> [ProgrammingLanguage] -> [ProgrammingLanguage]
+sortOnField fld dir = case fld of
+  SortName -> sortBy (.name)
+  SortDescription -> sortBy (.description)
+  SortFamily -> sortBy (.family)
+ where
+  sortBy :: (Ord b) => (ProgrammingLanguage -> b) -> [ProgrammingLanguage] -> [ProgrammingLanguage]
+  sortBy f = case dir of
+    Ascending -> sortOn f
+    Descending -> sortOn (Down . f)
 
-languagesView :: Maybe SortField -> [ProgrammingLanguage] -> View Languages ()
-languagesView fld langs =
+languagesView :: [ProgrammingLanguage] -> View Languages ()
+languagesView langs = do
+  mSt <- viewState
+  let directionOf fld = case mSt of
+        Just (f, dir) | f == fld -> Just dir
+        _ -> Nothing
+      sortColumn' lbl fld =
+        sortColumn (sortBtn lbl (SortOn fld) (directionOf fld))
   table langs ~ dataTable $ do
-    sortColumn (sortBtn "Language" (SortOn SortName) (fld == Just SortName)) (.name)
-    sortColumn (sortBtn "Family" (SortOn SortFamily) (fld == Just SortFamily)) $ \d -> pack $ show d.family
-    sortColumn (sortBtn "Description" (SortOn SortDescription) (fld == Just SortDescription)) (.description)
+    sortColumn' "Language" SortName (.name)
+    sortColumn' "Family" SortFamily $ \d -> pack $ show d.family
+    sortColumn' "Description" SortDescription (.description)
